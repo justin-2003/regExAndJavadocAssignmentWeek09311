1966345f2101708a930f03321ce607fd3ebc0a52
Justin Sam <jusitnsam02544@gmail.com>
2023-11-02 20:39:49 -0400

    intial

--- src/main/java/com/example/regexandjavadocassignmentweek09311/HelloController.java
+++ src/main/java/com/example/regexandjavadocassignmentweek09311/HelloController.java
@@ -4,22 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 
 public class HelloController {
-    @FXML
-    private TextField fn;
 
     @FXML
-    private TextField ln;
-
-    @FXML
-    private TextField email;
-
-    @FXML
-    private TextField dob;
-
-    @FXML
-    private TextField zipcode;
+    private TextField fn,ln,email,dob,zipcode;
 
     @FXML
     private Button Register;
@@ -27,15 +17,35 @@ public class HelloController {
     @FXML
     private Label label;
 
-    boolean flag;
+    boolean flag = false;
+    public void initialize() {
+        fn.setOnKeyPressed(event -> {
 
-    public void checksTheNameField(
-    ){
-        do {
-            String textField = " [a-zA-Z]{2,25}";
-            String input = fn.getText();
-            flag = input.matches(textField);
-        }while(!flag);
-        label.setText("First name should be only 2-25 characters");
+            if (event.getCode() != KeyCode.TAB && flag) {
+                fn.setStyle("-fx-border-color: black ; -fx-border-width: 1px ;");
+                label.setText("");
+
+                flag = false;
+            }
+        });
+        fn.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                System.out.println("Welcome text is focused");
+            } else {
+                if (fn.getText().matches("^(.+)@(.+)$*")) {
+                    fn.setEditable(false);
+                    fn.setBorder(null);
+                } else {
+
+                    fn.setStyle("-fx-border-color: red ; -fx-border-width: 4px ;");
+                    fn.setVisible(true);
+                    fn.requestFocus();
+                    label.setText(fn.getText() + " is not valid email");
+                    flag = true;
+                }
+
+            }
+        });
     }
 }
+
